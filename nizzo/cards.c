ae3390682fa292ad60500ce37a05fe8d18cc80f6
27Nizzo <afonso.martins8282@gmail.com>
2024-03-28 19:35:07 +0000

    seg fault :(

--- nizzo/cards.c
+++ nizzo/cards.c
@@ -22,37 +22,51 @@ typedef enum {
     Paus
 } Naipe;
 
-
 typedef struct {
     Valor value;
     Naipe suit;
 } Carta;
 
+const wchar_t* valores[] = {L"A", L"Dois", L"Tres", L"Quatro", L"Cinco", L"Seis", L"Sete", L"Oito", L"Nove", L"Dez", L"J", L"C", L"Q", L"K"};
+const wchar_t* naipes[] = {L"Espadas", L"Copas", L"Ouros", L"Paus"};
+
+void printCard(Carta c) {
+    wprintf(L"%ls de %ls\n", valores[c.value], naipes[c.suit]);
+}
 
 int compareCards(Carta c1, Carta c2) {
     if (c1.value > c2.value) 
     {
-        return 1;
+        wprintf(L"A maior carta é ");
+        printCard(c1);
+    } else if (c1.value < c2.value) {
+        wprintf(L"A maior carta é ");
+        printCard(c2);
+    } else if(c1.suit > c2.suit) {
+        wprintf(L"A maior carta é ");
+        printCard(c1);
     } else {
-        return 2;
-    }
-
-    if(c1.value == c2.value) {
-        if(c1.suit > c2.suit) {
-            return 1;
-        } else {
-            return 2; 
-        }
+        wprintf(L"A maior carta é ");
+        printCard(c2); 
     }
 
     return 0;
 }
 
-
 int main() {
-    Carta c1 = {A, Espadas};
-    Carta c2 = {K, Copas};
+    int numCards;
+    wprintf(L"Digite o número de cartas: ");
+    wscanf(L"%d", &numCards);
 
-    printf("%d\n", compareCards(c1, c2));
-    return 0;
+    Carta cards[numCards];
+    // Aqui você pode preencher o array 'cards' com as cartas que desejar
+
+    if(numCards >= 2) {
+        compareCards(cards[0], cards[1]);
+    } else {
+        for(int i = 0; i < numCards - 1; i++) {
+            compareCards(cards[i], cards[i+1]);
+        }
+    }
+    return 0; 
 }
